f3052282bf48be20b6702f2e5f4cb433f92d0664
mzinzun <zinzunmichael@gmail.com>
2025-04-09 09:31:20 -0700

    added caraousel images and content for meet god page

--- ghi/src/pageComponents/OneLess/content/Living.jsx
+++ ghi/src/pageComponents/OneLess/content/Living.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import 'react-tooltip/dist/react-tooltip.css'
---
+++ ghi/src/pageComponents/OneLess/content/meetGodData.js
@@ -0,0 +1,77 @@
+import mg2 from './one-less-assets/meetGod/mg2.jpg';
+import mg3 from './one-less-assets/meetGod/mg3.jpg';
+import mg4 from './one-less-assets/meetGod/mg4.jpg';
+import mg5 from './one-less-assets/meetGod/mg5.jpg';
+import mg6 from './one-less-assets/meetGod/mg6.png';
+
+export const slides = [
+    {
+        text: `And he passed in front of Moses, proclaiming, “The Lord, the Lord, the compassionate and gracious God, slow to anger, abounding in love and faithfulness, maintaining love to thousands, and forgiving wickedness, rebellion and sin…(Ex 34:6-7)`,
+        img: mg2
+    },
+    {
+        text: `I will give you hidden treasures, riches stored in secret places, so that you may know that I am the Lord, the God of Israel, who summons you by name. (Isa 45:3)`
+        ,
+        img: mg3
+    },
+    {
+        text: `I am the Lord, and there is no other; apart from me there is no God. I will strengthen you… (Isa 45:5)`,
+        img: mg4
+    },
+    {
+        text: `I form the light and create darkness, I bring prosperity and create disaster; I, the Lord, do all these things. (Isa 45:7)`,
+        img: mg5
+    },
+    {
+        text: `It is I who made the earth and created mankind on it. My own hands stretched out the heavens; I marshaled their starry hosts. (Isa 45:12)`,
+        img: mg6
+    },
+    {
+        text: ` “Turn to me and be saved, all you ends of the earth; for I am God, and there is no other. By myself I have sworn, my mouth has uttered in all integrity a word that will not be revoked: Before me every knee will bow; by me every tongue will swear. They will say of me, ‘In the Lord alone are deliverance and strength.’” All who have raged against him will come to him and be put to shame. (Isa 45:22-24)`,
+        img: mg2
+    },
+    {
+        text: `text:Even to your old age and gray hairs I am he, I am he who will sustain you. I have made you and I will carry you; I will sustain you and I will rescue you. (Isa 46:4)`,
+        img: mg3
+    },
+    {
+        text: `“Remember this, keep it in mind, take it to heart, you rebels. Remember the former things, those of long ago; I am God, and there is no other; I am God, and there is none like me. I make known the end from the beginning, from ancient times, what is still to come. I say, ‘My purpose will stand, and I will do all that I please.’ From the east I summon a bird of prey; from a far-off land, a man to fulfill my purpose. What I have said, that I will bring about; what I have planned, that I will do. (Isa 46:8-11)`,
+        img: mg4
+    },
+    {
+        text: `I am bringing my righteousness near, it is not far away; and my salvation will not be delayed. I will grant salvation to Zion, my splendor to Israel. (Isa 46:13)`,
+        img: mg5
+    },
+    {
+        text: `“Can a mother forget the baby at her breast and have no compassion on the child she has borne? Though she may forget, I will not forget you! See, I have engraved you on the palms of my hands; your walls are ever before me. (Isa 49:15-16)`,
+        img: mg6
+    },
+    {
+        text: `“For my thoughts are not your thoughts, neither are your ways my ways,” declares the Lord. As the heavens are higher than the earth, so are my ways higher than your ways and my thoughts than your thoughts. As the rain and the snow come down from heaven, and do not return to it without watering the earth and making it bud and flourish, so that it yields seed for the sower and bread for the eater, so is my word that goes out from my mouth: It will not return to me empty, but will accomplish what I desire and achieve the purpose for which I sent it. (Isa 55:8-11)`,
+        img: mg2
+    },
+    {
+        text: `“For I, the Lord, love justice; I hate robbery and wrongdoing. In my faithfulness I will reward my people and make an everlasting covenant with them. (Isa 61:8)`,
+        img: mg3
+    },
+    {
+        text: `The Lord appeared to us in the past, saying: “I have loved you with an everlasting love; I have drawn you with unfailing kindness. (Jer 31:3)`,
+        img: mg4
+    },
+    {
+        text: `“I am the Lord, the God of all mankind. Is anything too hard for me?” (Jer 32:27)`,
+        img: mg5
+    },
+    {
+        text: `I will give you a new heart and put a new spirit in you; I will remove from you your heart of stone and give you a heart of flesh. And I will put my Spirit in you and move you to follow my decrees and be careful to keep my laws. (Eze 36:26-27)`,
+        img: mg6
+    },
+    {
+        text: `“I the Lord do not change. So you, the descendants of Jacob, are not destroyed. (Mal 3:6)`,
+        img: mg2
+    },
+    {
+        text: `“I am the Alpha and the Omega,” says the Lord God, “who is, and who was, and who is to come, the Almighty.” (Rev 1:8)`,
+        Img: mg3
+    }
+]
